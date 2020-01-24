50d01c154da9b92e1663d43d5cfa0bf638356ab6
Kunal Gupta <ioskunal@gmail.com>
2020-01-25 00:43:06 +0530

    Alerts added

---
+++ GoogleDriveIntegration/Helpers/Alerts.swift
@@ -0,0 +1,27 @@
+
+//
+//  Alerts.swift
+//  GoogleDriveIntegration
+//
+//  Created by Kunal Gupta on 25/01/20.
+//  Copyright © 2020 Kunal Gupta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Alert {
+    
+    static func topMostVC() -> UIViewController? {
+        return UIApplication.shared.windows.last?.rootViewController
+    }
+    
+    static func show(title: String? = "Whoops!", message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction.init(title: "OK", style: .default) { (_) in}
+        alertController.addAction(alertAction)
+        if let vc = topMostVC() {
+            vc.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
