7781c995876be7aeb78ea7430ad08634ceade96a
Kunal Gupta <ioskunal@gmail.com>
2020-05-07 16:29:56 +0530

    Briding Header added.

---
+++ SupportingFiles/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  GoogleDriveIntegration
+//
+//  Created by Kunal Gupta on 07/05/20.
+//  Copyright © 2020 Kunal Gupta. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <GTMSessionFetcher/GTMSessionFetcher.h>
+#import <GTMSessionFetcher/GTMSessionFetcherService.h>
+
+
+#endif /* Bridging_Header_h */
